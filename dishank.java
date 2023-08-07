3ec8fefa2e0f5d55921fc6d127dc41014a0e7477
Dishank Shekokare <99533410+Dishank25S@users.noreply.github.com>
2023-08-07 11:29:00 -0700

    Add files via upload

---
+++ dishank.java
@@ -0,0 +1,14 @@
+// sum of n numbers using while 2.5.3
+import java.util.*; 
+public class dishank{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int sum = 0;
+int i = 1;
+while(i<=n){
+sum = sum + i;
+i = i + 1;
+}
+}
+}
